20d56697e6d1eaf0905c88f64b0f3f48117edd6e
Skat <55407440+skykatik@users.noreply.github.com>
2021-03-10 13:55:28 +0300

    Math command

--- src/main/java/inside/event/audit/AuditActionType.java
+++ src/main/java/inside/event/audit/AuditActionType.java
@@ -4,8 +4,8 @@ import discord4j.rest.util.Color;
 
 public enum AuditActionType{
     MESSAGE_EDIT(0x32cd32),
-    MESSAGE_CLEAR(0xdc143c),
     MESSAGE_DELETE(0xff341c),
+    MESSAGE_CLEAR(0xdc143c),
 
     VOICE_JOIN(0xff33cc),
     VOICE_LEAVE(0x9900cc),
@@ -13,9 +13,9 @@ public enum AuditActionType{
     USER_JOIN(0x6666cc),
     USER_LEAVE(0x6633cc),
     USER_KICK(0xffd37f),
+    USER_BAN(0xffcc00),
     USER_MUTE(0xffca59),
-    USER_UNMUTE(0x85ea88),
-    USER_BAN(0xffcc00);
+    USER_UNMUTE(0x85ea88);
 
     public final Color color;
 
