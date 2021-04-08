92986918410b2cec9facbc9c8ee213be4cf45412
skat <fishmoders@petruchio.org.ru>
2021-04-08 15:23:54 +0300

    Fix command finding, add avatar command

--- src/main/java/inside/service/InteractionCommandHandler.java
+++ src/main/java/inside/service/InteractionCommandHandler.java
@@ -1,9 +1,13 @@
 package inside.service;
 
-import inside.interaction.InteractionCommandEnvironment;
+import inside.interaction.*;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface InteractionCommandHandler{
 
     Mono<Void> handle(InteractionCommandEnvironment env);
+
+    List<InteractionCommand> getCommands();
 }
