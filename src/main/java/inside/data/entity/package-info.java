153d3b98aa18cf9e11bb9043b1c7c4926dba2022
skat <fishmoders@petruchio.org.ru>
2021-04-18 01:44:20 +0300

    Add Builder.Constructor annotations for builder-styled classes, add snowflake encode module

---
+++ src/main/java/inside/data/entity/package-info.java
@@ -0,0 +1,13 @@
+@Value.Style(
+        depluralize = true,
+        jdkOnly = true,
+        allParameters = true,
+        defaultAsDefault = true
+)
+@NonNullApi
+@SnowflakeEncodingEnabled
+package inside.data.entity;
+
+import inside.encoding.SnowflakeEncodingEnabled;
+import org.immutables.value.Value;
+import org.springframework.lang.NonNullApi;
