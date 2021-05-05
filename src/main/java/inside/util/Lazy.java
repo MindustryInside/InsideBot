5e94e36ec40b3a612b577986bd6d11cc98e32587
skat <fishmoders@petruchio.org.ru>
2021-05-05 16:49:39 +0300

    Add 'rm-cmd' test command, lazy initializing for js context

---
+++ src/main/java/inside/util/Lazy.java
@@ -0,0 +1,52 @@
+package inside.util;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class Lazy<T> implements Supplier<T>{
+    private final Supplier<T> delegate;
+    private volatile boolean initialized;
+    private T value;
+
+    private Lazy(Supplier<T> delegate){
+        this.delegate = Objects.requireNonNull(delegate, "delegate");
+    }
+
+    public static <T> Lazy<T> of(Supplier<T> delegate){
+        if(delegate instanceof Lazy){
+            return (Lazy<T>)delegate;
+        }
+        return new Lazy<>(delegate);
+    }
+
+    @Override
+    public T get(){
+        // A 2-field variant of Double Checked Locking.
+        if(!initialized){
+            synchronized(this){
+                if(!initialized){
+                    T t = delegate.get();
+                    value = t;
+                    initialized = true;
+                    return t;
+                }
+            }
+        }
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Lazy<?> that = (Lazy<?>)o;
+        return initialized == that.initialized &&
+                delegate.equals(that.delegate) &&
+                Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(delegate, initialized, value);
+    }
+}
