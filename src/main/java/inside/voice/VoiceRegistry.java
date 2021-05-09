ffd54ef123263a600c8ba511c407ed29211d6454
skat <fishmoders@petruchio.org.ru>
2021-05-09 17:13:32 +0300

    Base queue for tracks

--- src/main/java/inside/voice/VoiceRegistry.java
+++ src/main/java/inside/voice/VoiceRegistry.java
@@ -10,11 +10,14 @@ public class VoiceRegistry{
     private final Snowflake guildId;
     private final AudioPlayer player;
     private final AudioProvider audioProvider;
+    private final TrackLoader trackLoader;
 
     private VoiceRegistry(Snowflake guildId, AudioPlayer player, AudioProvider audioProvider){
         this.guildId = Objects.requireNonNull(guildId, "guildId");
         this.player = Objects.requireNonNull(player, "player");
         this.audioProvider = Objects.requireNonNull(audioProvider, "audioProvider");
+
+        trackLoader = new TrackLoader(player);
     }
 
     public static VoiceRegistry create(Snowflake guildId, AudioPlayer player, AudioProvider audioProvider){
@@ -31,5 +34,9 @@ public class VoiceRegistry{
 
     public AudioProvider getAudioProvider(){
         return audioProvider;
+    }
+
+    public TrackLoader getTrackLoader(){
+        return trackLoader;
     }
 }
