266c210a6c8d7f01013c7565a742c7685547516e
skat <fishmoders@petruchio.org.ru>
2021-06-11 16:35:55 +0300

    Several cleanup

--- src/main/java/inside/util/codec/Utf8.java
+++ src/main/java/inside/util/codec/Utf8.java
@@ -5,14 +5,12 @@ import java.nio.charset.*;
 
 public final class Utf8{
 
-    private static final Charset CHARSET = StandardCharsets.UTF_8;
-
     private Utf8(){
     }
 
     public static byte[] encode(CharSequence string){
         try{
-            ByteBuffer bytes = CHARSET.newEncoder().encode(CharBuffer.wrap(string));
+            ByteBuffer bytes = StandardCharsets.UTF_8.newEncoder().encode(CharBuffer.wrap(string));
             byte[] bytesCopy = new byte[bytes.limit()];
             System.arraycopy(bytes.array(), 0, bytesCopy, 0, bytes.limit());
             return bytesCopy;
@@ -23,7 +21,7 @@ public final class Utf8{
 
     public static String decode(byte[] bytes){
         try{
-            return CHARSET.newDecoder().decode(ByteBuffer.wrap(bytes)).toString();
+            return StandardCharsets.UTF_8.newDecoder().decode(ByteBuffer.wrap(bytes)).toString();
         }catch(CharacterCodingException ex){
             throw new IllegalArgumentException("Decoding failed", ex);
         }
