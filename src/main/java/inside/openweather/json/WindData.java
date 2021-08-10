c6cc92ed7bd5cb404163c713a5511e6a0001946c
skat <fishmoders@petruchio.org.ru>
2021-08-10 10:15:07 +0300

    Make wind-gust possible

--- src/main/java/inside/openweather/json/WindData.java
+++ src/main/java/inside/openweather/json/WindData.java
@@ -2,6 +2,7 @@ package inside.openweather.json;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.*;
+import discord4j.discordjson.possible.Possible;
 import org.immutables.value.Value;
 
 @Value.Immutable
@@ -18,5 +19,5 @@ public interface WindData{
     @JsonProperty("deg")
     int degrees();
 
-    float gust();
+    Possible<Float> gust();
 }
